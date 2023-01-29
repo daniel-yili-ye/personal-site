eb108bb4b37430c9d63daf178ffe83649f469072
Daniel Ye <daniel.ye@live.com>
2023-01-28 23:53:03 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import dye from "./assets/dye_face.jpeg";
+import Light from "./assets/Light";
+import Dark from "./assets/Dark";
+import Twitter from "./assets/Twitter";
+import Github from "./assets/Github";
+import LinkedIn from "./assets/LinkedIn";
+
+function App() {
+  const [darkMode, setDarkMode] = useState(isDark);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  function isDark() {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  return (
+    <div className="container mx-auto py-12 max-w-xl space-y-6">
+      <div className="sm:flex space-y-6 sm:space-y-0 sm:space-x-6 px-6 justify-center items-center">
+        <img className="rounded-xl sm:w-52" src={dye} alt="Daniel Ye" />
+        <div className="flex flex-col space-y-6 text-left">
+          <h1 className="font-semibold text-3xl">👋 Hello, I'm Daniel Ye</h1>
+          <p className="text-neutral-500 dark:text-neutral-300">
+            <span className="font-bold">Software Engineer</span> by day,{" "}
+            <span className="font-bold">CPA</span> by training,{" "}
+            <span className="font-bold">Christian</span> by life. I love numbers
+            and code, but also enjoy a good book and a swim to unwind. Let's
+            create something great together!
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-center items-center gap-6">
+        <Twitter></Twitter>
+        <Github></Github>
+        <LinkedIn></LinkedIn>
+        <button
+          type="button"
+          aria-label="Toggle dark mode"
+          className="p-2 transition rounded-full shadow-lg bg-white/90 shadow-neutral-800/10 ring-1 ring-neutral-900/5 backdrop-blur dark:bg-gray-800/90 dark:ring-white/10 dark:hover:ring-white/20"
+          onClick={() => {
+            setDarkMode(!darkMode);
+          }}
+        >
+          {darkMode ? <Dark></Dark> : <Light></Light>}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    container: {
+      padding: {
+        // DEFAULT: "1rem",
+        // lg: "10rem",
+        // xl: "20rem",
+      },
+    },
+  },
+  plugins: [],
+  darkMode: "class",
+};
